2ba11674cb6d5bb3091e799be4765146d586d6aa
David Tran <109535790+david125tran@users.noreply.github.com>
2023-12-26 10:01:46 -0500

    Add files via upload

---
+++ Section03/Basic Operations in R.R
@@ -0,0 +1,33 @@
+# Adding two numbers
+1+1
+
+# Subtract two numbers
+10-5
+
+# Multiplication
+10*7
+
+# Division
+10/4
+
+# Add parenthesis
+10/(5/5)
+
+# Square root
+sqrt(9)
+
+# Exponentials
+exp(10)
+
+# Logarithm
+log(1)
+log(4,2)
+
+# See documentation of a function
+?sqrt()
+
+# Misc
+sqrt(85)
+15/(10^2)
+(215 + log(100))/exp(100)
+log(24)
