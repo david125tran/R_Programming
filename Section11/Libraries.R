59a6c92ecfeaa5c833ffba45d1489b2f3c08fa9d
David Tran <109535790+david125tran@users.noreply.github.com>
2024-01-02 16:55:07 -0500

    Add files via upload

---
+++ Section11/Libraries.R
@@ -0,0 +1,12 @@
+# ------------------- Installing libraries ------------------- 
+install.packages("ggplot2")
+install.packages("xlsx")
+
+# Alternatively:
+# Tools --> Install Packages...
+
+# ------------------- Loading libraries ------------------- 
+library("ggplot2")
+
+# ------------------- Using libraries ------------------- 
+ggplot()
